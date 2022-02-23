8e8685d9c13cd078c07f4a6b9a96c9d6d641a5e9
Roman Ojha <razzroman98@gmail.com>
2022-02-23 12:53:58 +0545

    completed designing page footer

--- client/src/react-components/Footer.js
+++ client/src/react-components/Footer.js
@@ -3,6 +3,10 @@ import "./styles/footer.css";
 
 const Footer = () => {
   const year = new Date().getFullYear();
+  const instaGram = "https://www.instagram.com/roman__ojha/";
+  const github = "https://github.com/Roman-Ojha";
+  const linkedin = "https://www.linkedin.com/in/roman-ojha-3b8bb2209/";
+  const twitter = "https://twitter.com/RomanOjha1";
   return (
     <>
       <div className="Footer_Container">
@@ -13,9 +17,46 @@ const Footer = () => {
             <h2>Privacy Policy</h2>
           </div>
           <div className="FollowUs_Container">
-            <span className="iconify" data-icon="mdi:instagram"></span>
-            <span className="iconify" data-icon="mdi:twitter"></span>
-            <span className="iconify" data-icon="mdi:linkedin"></span>
+            <div
+              onClick={() => {
+                window.open(github);
+              }}
+            >
+              <span
+                className="iconify FollowUs_Icons"
+                data-icon="mdi:github"
+              ></span>
+            </div>
+            <div
+              onClick={() => {
+                window.open(instaGram);
+              }}
+            >
+              <span
+                className="iconify FollowUs_Icons"
+                data-icon="mdi:instagram"
+              ></span>
+            </div>
+            <div
+              onClick={() => {
+                window.open(twitter);
+              }}
+            >
+              <span
+                className="iconify FollowUs_Icons"
+                data-icon="mdi:twitter"
+              ></span>
+            </div>
+            <div
+              onClick={() => {
+                window.open(linkedin);
+              }}
+            >
+              <span
+                className="iconify FollowUs_Icons"
+                data-icon="mdi:linkedin"
+              ></span>
+            </div>
           </div>
         </div>
         <h1 className="Copyright_Text">Copyright &copy; {year} Roman Ojha</h1>
